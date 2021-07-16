300def4fd6431db2c3c60d27cbaec1b5e64a1317
Luis Winder <lvwinder@gmail.com>
2021-07-16 12:34:03 +0100

    installed mongoose and dotenv

---
+++ src/db/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+    } catch (error) {
+        console.log(error)
+    }
+}
